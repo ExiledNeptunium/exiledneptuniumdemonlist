a6341f4550ffd1948eaa19e571ed629dcec44def
ExiledNeptunium <163695717+ExiledNeptunium@users.noreply.github.com>
2025-08-09 20:07:58 -0600

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
+import PList from './pages/PList.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/demons', component: List },
+    { path: '/pemons', component: PList },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
