8ebe9abad0f4a5fd016cb93d0b36874e67de7807
ExiledNeptunium <163695717+ExiledNeptunium@users.noreply.github.com>
2025-08-09 20:01:30 -0600

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -5,7 +5,7 @@ import PList from '.pages/PList.js';
 
 export default [
     { path: '/demons', component: List },
-    { path: '/pemons', component: PList},
+    { path: '/pemons', component: PList },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
