9f403c76a6f1acd7b049b71740afadc9f164cff7
ExiledNeptunium <163695717+ExiledNeptunium@users.noreply.github.com>
2025-08-09 19:57:40 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,29 +8,6 @@ export const store = Vue.reactive({
         localStorage.setItem('dark', JSON.stringify(this.dark));
     },
 });
-
-(async () => {
-    // If the URL is bare or root, redirect to first level
-    const hash = window.location.hash;
-    if (hash === '' || hash === '#/') {
-        const list = await fetchList();
-        const firstLevelName = list?.[0]?.[0]?.name;
-        if (firstLevelName) {
-            const encoded = encodeURIComponent(firstLevelName);
-            window.location.hash = `#/list/${encoded}`;
-        }
-    }
-
-    // Init app only after checking or redirecting
-    const app = Vue.createApp({
-        data: () => ({ store }),
-    });
-
-    const router = VueRouter.createRouter({
-        history: VueRouter.createWebHashHistory(),
-        routes,
-    });
-
     app.use(router);
     app.mount('#app');
 })();
